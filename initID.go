163d287c1104eecbf0762703112c7306d07ffe04
TungCheng <ga2006088445@gmail.com>
2021-06-12 07:00:27 +0800

    (^) 每次工作id處理以及紀錄

---
+++ initID.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var thisID string = "0"
+
+//初始化這次ID
+func infoID() {
+	thisID = strconv.Itoa(rand.New(rand.NewSource(time.Now().UnixNano())).Int())
+	setLog("infoID", "初始化本次工作ID", thisID)
+}
