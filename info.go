69491895ca10d5c420dd9190c7204c219f32b878
TungCheng <ga2006088445@gmail.com>
2021-06-12 06:58:00 +0800

    (^) 修正infoScreen

--- info.go
+++ info.go
@@ -10,13 +10,14 @@ var infoh int = 2000
 //手機板 900*1600
 
 //初始化本程式未來要找的座標範圍 (不找全圖)
-func infoScreen() bool {
-	succ, x, y := whilescreen(GetSystemImg("noxLogo.png"), 0.05)
+//需要有initImg.png  x:掃描範圍x與initImg.png位置偏移量 y:掃描範圍y與initImg.png位置偏移量 w:寬度 h:高度
+func infoScreen(x int, y int, w int, h int) bool {
+	succ, xx, yy := whilescreen(GetSystemImg("initImg.png"), 0.05)
 	if succ {
-		infox = x - 40
-		infoy = y - 40
-		infow = 600
-		infoh = 1050
+		infox = xx + x
+		infoy = yy + y
+		infow = w
+		infoh = h
 		setLog("infoScreen", "設定視窗位置成功", fmt.Sprintf("%d, %d, %d, %d", infox, infoy, infow, infoh))
 		return true
 	}
