6023556eb896a26c86e49d7cb4376ea62ecc9b7e
TungCheng <ga2006088445@gmail.com>
2021-06-12 10:04:35 +0800

    (^) 掃圖增加count功能

--- utils-screeen-base.go
+++ utils-screeen-base.go
@@ -9,7 +9,7 @@ import (
 
 var rigorousSec = 1
 
-//搜尋一張圖片 符合回傳true,
+//搜尋多張圖片 只要符合一張,
 //frequency : 要搜尋幾次,
 //matchNumber : 圖片符合的基準 越靠近1越容易匹配,
 //rigorous : 是否需要持續1秒鐘以上圖片存在才算匹配成功 秒數可由rigorousSec更改,
@@ -21,50 +21,54 @@ func findOneImages(frequency int, matchNumber float32, rigorous bool, imgFullPat
 	index = -1
 	x = -1
 	y = -1
+	tmp_frequency := 0
+	for tmp_frequency < frequency {
+		robotgo.MoveMouse(0, 0)
+		new_bitmap := robotgo.CaptureScreen(infox, infoy, infow, infoh)
+		defer robotgo.FreeBitmap(new_bitmap)
 
-	robotgo.MoveMouse(0, 0)
-	new_bitmap := robotgo.CaptureScreen(infox, infoy, infow, infoh)
-	defer robotgo.FreeBitmap(new_bitmap)
-
-	rigorous_bitmap := robotgo.CaptureScreen(0, 0, 0, 0)
-	if rigorous {
-		robotgo.Sleep(rigorousSec)
-		rigorous_bitmap = robotgo.CaptureScreen(infox, infoy, infow, infoh)
-	}
-	defer robotgo.FreeBitmap(rigorous_bitmap)
-
-	for idx, imgFullPath := range imgFullPaths {
-		fileExist, imgw, imgh := FileExist(imgFullPath)
-		if !fileExist {
-			continue
-		}
-		dst_map := robotgo.OpenBitmap(imgFullPath)
-		defer robotgo.FreeBitmap(dst_map)
-
-		fx, fy := robotgo.FindBitmap(dst_map, new_bitmap, matchNumber)
-		//如果沒找到就下一張圖
-		if fx == -1 && fy == -1 {
-			continue
-		}
-		//如果需要嚴謹 就在找一次 如果沒有 一樣進入下張圖
+		rigorous_bitmap := robotgo.CaptureScreen(0, 0, 0, 0)
 		if rigorous {
-			fx, fy = robotgo.FindBitmap(dst_map, rigorous_bitmap, matchNumber)
+			robotgo.Sleep(rigorousSec)
+			rigorous_bitmap = robotgo.CaptureScreen(infox, infoy, infow, infoh)
+		}
+		defer robotgo.FreeBitmap(rigorous_bitmap)
+
+		for idx, imgFullPath := range imgFullPaths {
+			fileExist, imgw, imgh := FileExist(imgFullPath)
+			if !fileExist {
+				continue
+			}
+			dst_map := robotgo.OpenBitmap(imgFullPath)
+			defer robotgo.FreeBitmap(dst_map)
+
+			fx, fy := robotgo.FindBitmap(dst_map, new_bitmap, matchNumber)
+			//如果沒找到就下一張圖
 			if fx == -1 && fy == -1 {
 				continue
 			}
+			//如果需要嚴謹 就在找一次 如果沒有 一樣進入下張圖
+			if rigorous {
+				fx, fy = robotgo.FindBitmap(dst_map, rigorous_bitmap, matchNumber)
+				if fx == -1 && fy == -1 {
+					continue
+				}
+			}
+			succ = true
+			index = idx
+			x = fx + (imgw / 2) + infox
+			y = fy + (imgh / 2) + infoy
+			log.Infof("findOneImages %s is found", imgFullPath)
+			setLog("findOneImages", "尋找圖片成功", fmt.Sprintf("img: %s, x:%d, y:%d", imgFullPath, x, y))
+			return
 		}
-		succ = true
-		index = idx
-		x = fx + (imgw / 2) + infox
-		y = fy + (imgh / 2) + infoy
-		log.Infof("findOneImages %s is found", imgFullPath)
-		setLog("findOneImages", "尋找圖片成功", fmt.Sprintf("img: %s, x:%d, y:%d", imgFullPath, x, y))
-		return
+		robotgo.Sleep(1)
+		tmp_frequency = tmp_frequency + 1
 	}
 	return
 }
 
-//搜尋多張圖片 都符合回傳true,
+//搜尋多張圖片 要尋找多張,
 //frequency : 要搜尋幾次,
 //matchNumber : 圖片符合的基準 越靠近1越容易匹配,
 //rigorous : 是否需要持續1秒鐘以上圖片存在才算匹配成功 : 秒數可由rigorousSec修改,
@@ -79,36 +83,42 @@ func findAllImages(frequency int, matchNumber float32, rigorous bool, imgFullPat
 		return
 	}
 
-	robotgo.MoveMouse(0, 0)
-	new_bitmap := robotgo.CaptureScreen(infox, infoy, infow, infoh)
-	defer robotgo.FreeBitmap(new_bitmap)
+	tmp_frequency := 0
 
-	rigorous_bitmap := robotgo.CaptureScreen(0, 0, 0, 0)
-	if rigorous {
-		robotgo.Sleep(rigorousSec)
-		rigorous_bitmap = robotgo.CaptureScreen(infox, infoy, infow, infoh)
-	}
-	defer robotgo.FreeBitmap(rigorous_bitmap)
+	for tmp_frequency < frequency {
+		robotgo.MoveMouse(0, 0)
+		new_bitmap := robotgo.CaptureScreen(infox, infoy, infow, infoh)
+		defer robotgo.FreeBitmap(new_bitmap)
 
-	for _, imgFullPath := range imgFullPaths {
-		dst_map := robotgo.OpenBitmap(imgFullPath)
-		defer robotgo.FreeBitmap(dst_map)
-
-		fx, fy := robotgo.FindBitmap(dst_map, new_bitmap, matchNumber)
-		//如果沒找到就下一張圖
-		if fx == -1 && fy == -1 {
-			return
-		}
-		//如果需要嚴謹 就在找一次 如果沒有 一樣進入下張圖
+		rigorous_bitmap := robotgo.CaptureScreen(0, 0, 0, 0)
 		if rigorous {
-			fx, fy = robotgo.FindBitmap(dst_map, rigorous_bitmap, matchNumber)
-			if fx == -1 && fy == -1 {
-				return
-			}
+			robotgo.Sleep(rigorousSec)
+			rigorous_bitmap = robotgo.CaptureScreen(infox, infoy, infow, infoh)
 		}
-		log.Infof("findAllImages %s is found", imgFullPath)
-		setLog("findAllImages", "尋找圖片成功", fmt.Sprintf("img: %s", imgFullPath))
+		defer robotgo.FreeBitmap(rigorous_bitmap)
+
+		for _, imgFullPath := range imgFullPaths {
+			dst_map := robotgo.OpenBitmap(imgFullPath)
+			defer robotgo.FreeBitmap(dst_map)
+
+			fx, fy := robotgo.FindBitmap(dst_map, new_bitmap, matchNumber)
+			//如果沒找到就離開這次找圖 等待一秒下一次再一次掃圖加找圖
+			if fx == -1 && fy == -1 {
+				break
+			}
+			//如果需要嚴謹 就在找一次 如果沒找到就離開這次找圖 等待一秒下一次再一次掃圖加找圖
+			if rigorous {
+				fx, fy = robotgo.FindBitmap(dst_map, rigorous_bitmap, matchNumber)
+				if fx == -1 && fy == -1 {
+					break
+				}
+			}
+			log.Infof("findAllImages %s is found", imgFullPath)
+			setLog("findAllImages", "尋找圖片成功", fmt.Sprintf("img: %s", imgFullPath))
+		}
+		tmp_frequency = tmp_frequency + 1
+		succ = true
+		return
 	}
-	succ = true
 	return
 }
