9c41eddae7eaded87b3a327ce7b15131a461f387
TungCheng <ga2006088445@gmail.com>
2021-06-12 10:39:08 +0800

    (^) 整理完滑鼠功能

--- utils-mouse.go
+++ utils-mouse.go
@@ -2,71 +2,46 @@ package main
 
 import "github.com/go-vgo/robotgo"
 
-func leftMouseClick() {
+//如果符合其中一張圖 就執行滑鼠左鍵,
+//frequency : 要搜尋幾次,
+//matchNumber : 圖片符合的基準 越靠近1越容易匹配,
+//rigorous : 是否需要持續1秒鐘以上圖片存在才算匹配成功 : 秒數可由rigorousSec修改,
+//imgFullPaths : 傳入圖片相對全路徑 (可多張)
+func haveOneImgsRight(frequency int, matchNumber float32, rigorous bool, imgFullPaths ...string) {
+	findSucc, _, x, y := findOneImages(frequency, matchNumber, rigorous, imgFullPaths...)
+	if findSucc {
+		rightMouseClick(x, y)
+	}
+}
+
+//如果符合其中一張圖 就執行滑鼠右鍵,
+//frequency : 要搜尋幾次,
+//matchNumber : 圖片符合的基準 越靠近1越容易匹配,
+//rigorous : 是否需要持續1秒鐘以上圖片存在才算匹配成功 : 秒數可由rigorousSec修改,
+//imgFullPaths : 傳入圖片相對全路徑 (可多張)
+func haveOneImgsLeft(frequency int, matchNumber float32, rigorous bool, imgFullPaths ...string) {
+	findSucc, _, x, y := findOneImages(frequency, matchNumber, rigorous, imgFullPaths...)
+	if findSucc {
+		leftMouseClick(x, y)
+	}
+}
+
+func customizedMouseLeft() {
 	robotgo.MouseClick("left")
+	robotgo.Sleep(1)
 }
 
-func rightMouseClick() {
+func customizedMouseRight() {
 	robotgo.MouseClick("right")
+	robotgo.Sleep(1)
 }
 
-//左鍵點擊多圖片的其中一張正中間 |
-//fullImgs 多圖片路徑 |
-//matchNumber 匹配程度
-func leftMouseClickImgMany(matchNumber float64, fullImgs ...string) bool {
-
-	for _, fullimg := range fullImgs {
-		succ, x, y := whilescreenbase(fullimg, 2, matchNumber)
-		if succ {
-			robotgo.MoveMouse(x, y)
-			leftMouseClick()
-			return true
-		}
-	}
-	return false
+func leftMouseClick(x int, y int) {
+	robotgo.MoveMouse(x, y)
+	customizedMouseLeft()
 }
 
-//左鍵點擊圖片正中間 |
-//fullimg 圖片路徑 |
-//args[0] 匹配程度
-func leftMouseClickImg(fullimg string, args ...float64) bool {
-	matchNumber := 0.01
-	if len(args) == 1 {
-		matchNumber = args[0]
-	}
-
-	succ, x, y := whilescreen(fullimg, matchNumber)
-	if succ {
-		robotgo.MoveMouse(x, y)
-		leftMouseClick()
-		return true
-	}
-	return false
-}
-
-//找到圖後 點選另一張圖 |
-// findImg : 如果看到這張圖 |
-// clickImg : 點選這張圖 |
-// args[0] : find匹配程度 |
-// args[1] : click匹配程度
-func checkImgClickOtherImg(findImg string, clickImg string, args ...interface{}) bool {
-	matchNumberFind := 0.01
-	matchNumberClick := 0.01
-	if len(args) >= 1 && args[0] != nil {
-		matchNumberFind, _ = args[0].(float64)
-	}
-	if len(args) >= 1 && args[1] != nil {
-		matchNumberClick, _ = args[1].(float64)
-	}
-
-	skipGameSucc := findscreen(findImg, matchNumberFind)
-	if skipGameSucc {
-		skipOKSucc := leftMouseClickImg(clickImg, matchNumberClick)
-		if !skipOKSucc {
-			return false
-		}
-	} else {
-		return false
-	}
-	return true
+func rightMouseClick(x int, y int) {
+	robotgo.MoveMouse(x, y)
+	customizedMouseRight()
 }
--- utils-text.go
+++ utils-text.go
@@ -3,29 +3,12 @@ package main
 import "github.com/go-vgo/robotgo"
 
 func textLocation(x int, y int, text string) (succ bool) {
-
 	robotgo.MoveMouse(x, y)
 	robotgo.MouseClick()
 	robotgo.Sleep(1)
 	robotgo.MouseClick()
 	robotgo.Sleep(1)
 	robotgo.TypeStr(text, 10)
-
-	succ = true
-	return
-
-}
-func textLocationPwd(x int, y int, text string) (succ bool) {
-
-	robotgo.MoveMouse(x, y)
-	robotgo.MouseClick()
-	robotgo.Sleep(1)
-	robotgo.MouseClick()
-	for _, v := range text {
-		robotgo.Sleep(1)
-		robotgo.KeyTap(string(v))
-	}
-
 	succ = true
 	return
 
