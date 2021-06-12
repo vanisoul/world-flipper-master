e94a2baddfb6f288e79e00e74b4456fc2f01416d
TungCheng <ga2006088445@gmail.com>
2021-06-12 11:11:41 +0800

    (^) 增加一直處理某件事情 直到發現圖片

--- utils-screen.go
+++ utils-screen.go
@@ -11,6 +11,38 @@ import (
 	"github.com/labstack/gommon/log"
 )
 
+//一直執行func 當遇到圖片其一
+//funcs : 被執行的方法,
+//frequency : 要搜尋幾次,
+//matchNumber : 圖片符合的基準 越靠近1越容易匹配,
+//rigorous : 是否需要持續1秒鐘以上圖片存在才算匹配成功 : 秒數可由rigorousSec修改,
+//imgFullPaths : 傳入圖片相對全路徑 (多張)
+func findOneImgFuncStop(funcs func(), frequency int, matchNumber float32, rigorous bool, imgFullPaths ...string) {
+	for {
+		funcs()
+		findSucc, _, _, _ := findOneImages(frequency, matchNumber, rigorous, imgFullPaths...)
+		if findSucc {
+			return
+		}
+	}
+}
+
+//一直執行func 當遇到圖片全部
+//funcs : 被執行的方法,
+//frequency : 要搜尋幾次,
+//matchNumber : 圖片符合的基準 越靠近1越容易匹配,
+//rigorous : 是否需要持續1秒鐘以上圖片存在才算匹配成功 : 秒數可由rigorousSec修改,
+//imgFullPaths : 傳入圖片相對全路徑 (多張)
+func findAllImgFuncStop(funcs func(), frequency int, matchNumber float32, rigorous bool, imgFullPaths ...string) {
+	for {
+		funcs()
+		findSucc := findAllImages(frequency, matchNumber, rigorous, imgFullPaths...)
+		if findSucc {
+			return
+		}
+	}
+}
+
 //如果符合其中一張圖 就執行匹配到的第幾個func => func 有x y用為找到圖片的中心點,
 //frequency : 要搜尋幾次,
 //matchNumber : 圖片符合的基準 越靠近1越容易匹配,
